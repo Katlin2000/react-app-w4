1250c30367d175da79110fd56859e04288b49589
Katlin2000 <katlin.pala@gmail.com>
2025-07-02 20:59:35 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import SearchEngine from "./SearchEngine";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <div>
+        <SearchEngine />
+      </div>
+    </div>
+  );
+}
