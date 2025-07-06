42a30fb3c29001a2438f05203cbd609691a5d445
Katlin2000 <katlin.pala@gmail.com>
2025-07-06 17:40:10 +0100

    added link to github

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ export default function App() {
       <div>
         <SearchEngine />
       </div>
+      <p>
+        This project was coded by Katlin Pala and is{" "}
+        <a href="https://github.com/Katlin2000/react-app-w4">
+          open source on Github
+        </a>{" "}
+        and{" "}
+        <a href="https://react-homework-w4.netlify.app/">hosted on Netlify</a>
+      </p>
     </div>
   );
 }
